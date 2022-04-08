ee05d7cffb9d76d3f9725752c142ff517412c6ad
Robo16-cmd <84016136+Robo16-cmd@users.noreply.github.com>
2022-04-08 19:36:06 +0100

    Create object.js
    javascript object assignment

---
+++ object.js
@@ -0,0 +1,42 @@
+// 1. Create an object that has properties "username" and "password". Fill those values in with strings.
+
+var user = {
+
+  username: "Tamara",
+  password: "password1"
+
+};
+
+console.log(user)
+ // 2. Create an array which contains the object you have made above and name the array "database".
+var database = [
+    {
+        username: "Tamara",
+        password: "password1",
+    
+    }
+
+]
+
+console.log(database)
+// // 3. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+
+    var newsfeed = [
+        {
+            "username": "Sample1",
+            "timeline": "By tommorow",
+            
+          },
+
+          {
+            "username": "Sample2",
+            "timeline": "By Saturday",
+          },
+          {
+            "username": "Sample3",
+            "timeline": "By Sunday",
+          }
+
+    ]
+
+    console.log(newsfeed);
